dedc8a855508fbca315e2104533dec4d5433aeb2
eduAlmeida1 <eduardo.almeida@cadmus.com.br>
2021-04-14 11:03:05 -0300

    a basic auth api

---
+++ src/app/middleware/authMiddleware.ts
@@ -0,0 +1,35 @@
+/* eslint-disable consistent-return */
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface TokenPayload {
+  id: string;
+  iat: number;
+  exp: number;
+}
+
+export default function authMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const { authorization } = request.headers;
+
+  if (!authorization) {
+    return response.sendStatus(401);
+  }
+
+  const token = authorization.replace('Bearer', '').trim();
+
+  try {
+    const data = jwt.verify(token, 'secret');
+
+    const { id } = data as TokenPayload;
+
+    request.userId = id;
+
+    return next();
+  } catch (error) {
+    return response.sendStatus(401).json({ error });
+  }
+}
---
+++ src/database/migrations/1618357530060-CreateUsersTable.ts
@@ -0,0 +1,38 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsersTable1618357530060 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"');
+  }
+}
